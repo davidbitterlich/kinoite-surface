0eba5833240ff02ba32e8b0640828235ad7fb900
David Bitterlich <d.bitterlich@raynet.de>
2026-04-22 13:07:26 +0200

    Add surface kernel and remove unnecessary comments and data from readme

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,17 @@
 
 set -ouex pipefail
 
+### Remove Ublue-Kernel
+rpm --erase kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+
+### Add the surface kernel repository
+dnf5 config-manager addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
+
+### Install surface packages
+dnf5 install --allowerasing kernel-surface iptsd libwacom-surface surface-secureboot
+
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
 dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
 
 systemctl enable podman.socket
