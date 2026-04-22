30cc8b0ceb0c848bdb7a87ee4561f591103b238a
David Bitterlich <d.bitterlich@raynet.de>
2026-04-22 13:14:17 +0200

    Remove virtualbox-guest-additions from the image

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 ### Remove Ublue-Kernel
-rpm --erase kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+rpm --erase kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra virtualbox-guest-additions
 
 ### Add the surface kernel repository
 dnf5 config-manager addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
