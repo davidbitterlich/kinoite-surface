c58accabb81c66c39c35a62bdd2352eea97d2bba
David Bitterlich <d.bitterlich@raynet.de>
2026-04-22 13:33:49 +0200

    Apply dracut workaround

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,14 +2,32 @@
 
 set -ouex pipefail
 
-### Remove Ublue-Kernel
-rpm --erase kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra virtualbox-guest-additions
-
 ### Add the surface kernel repository
 dnf5 config-manager addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
 
+### disable kernel-install hooks
+pushd /usr/lib/kernel/install.d
+
+mv 05-rpmostree.install 05-rpmostree.install.bak
+mv 50-dracut.install 50-dracut.install.bak
+
+printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
+printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
+
+chmod +x 05-rpmostree.install 50-dracut.install
+
+popd
+
 ### Install surface packages
 dnf5 install --allowerasing -y kernel-surface iptsd libwacom-surface surface-secureboot
+
+# Restore kernel-install
+pushd /usr/lib/kernel/install.d
+
+mv -f 05-rpmostree.install.bak 05-rpmostree.install
+mv -f 50-dracut.install.bak 50-dracut.install
+
+popd
 
 ### Install packages
 
