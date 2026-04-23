d1c59e50ec9004fc7a5b9e064fd276b9e066ae56
David Bitterlich <d.bitterlich@raynet.de>
2026-04-23 07:38:17 +0200

    Try to fix dracut

--- build_files/finalize.sh
+++ build_files/finalize.sh
@@ -6,6 +6,10 @@ KVER=$(rpm -q --queryformat="%{VERSION}-%{RELEASE}.%{ARCH}" kernel-surface-core)
 
 export DRACUT_NO_XATTR=1
 
+mkdir -p /var/tmp
+chmod 1777 /var/tmp
+
+sed -i '/\/root/d' /etc/dracut.conf.d/*.conf || true
 dracut \
   --no-hostonly \
   --kver "${KVER}" \
